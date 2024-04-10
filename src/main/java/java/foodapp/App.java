cbe25d1c357a7463c74aea8a56800d4c0e8066f5
nivas0605 <nivas66ravikumar@gmail.com>
2024-04-10 10:54:15 -0400

    food

---
+++ src/main/java/java/foodapp/App.java
@@ -0,0 +1,13 @@
+package java.foodapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
